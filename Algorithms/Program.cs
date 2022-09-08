d9b06f6cb123285b951fe6a8d44a14d3d61e47e8
PsychoMensis <103057687+PsychoMensis@users.noreply.github.com>
2022-09-08 15:02:40 +0300

    Selection sort added

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -12,24 +12,10 @@ namespace Algorithms
         
         public static void Main(string[] args)
         {
-            int [] BS = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            int [] BS = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 1,  12, 13 };
 
-            WeightedQuickUnion quickUnion = new WeightedQuickUnion();
-
-            quickUnion.QuickFindUF(10);
-            quickUnion.Union(4, 3);
-            quickUnion.Union(3, 8);
-            quickUnion.Union(6, 5);
-            quickUnion.Union(9, 4);
-            quickUnion.Union(2, 1);
-            quickUnion.Union(9, 4);
-            quickUnion.Union(5, 0);
-            quickUnion.Union(7, 2);
-            quickUnion.Union(6, 1);
-            quickUnion.Union(7, 3);
-
-            Console.WriteLine(quickUnion.Connected(8, 9));
-            Console.WriteLine(quickUnion);
+            SelectionSort sort = new SelectionSort();
+            int[] sorted = sort.SelSort(BS);
 
         }
     }
---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class SelectionSort
+    {
+        public int FindSmallest(int[] arr)
+        {
+            int smallest = arr[0];
+            int smallestIndex = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                    smallestIndex = i;
+                }
+            }
+            return smallestIndex;
+
+           
+        }
+        public int[] SelSort(int[] arr)
+        {
+            int [] SortedArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int smallest = FindSmallest(arr);
+                SortedArr[i] = arr[smallest];
+                arr[smallest] = int.MaxValue;
+            }
+            return SortedArr;
+        }
+    }
+}
