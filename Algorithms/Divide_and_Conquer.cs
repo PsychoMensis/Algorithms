aebaef584b06aa8a2cdb4151c9ecb239a56f03be
PsychoMensis <103057687+PsychoMensis@users.noreply.github.com>
2022-09-08 15:33:33 +0300

    Divide and Conquer algorithm for recursive Sum added.

---
+++ Algorithms/Divide_and_Conquer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class Divide_and_Conquer
+    {
+        public int Sum(int[] arr, int length)
+        {
+            int i = length-1;
+            if (i == 0) return arr[0];
+            return arr[i] + Sum(arr, i);
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -13,10 +13,15 @@ namespace Algorithms
         public static void Main(string[] args)
         {
             int [] BS = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 1,  12, 13 };
+            int[] BS2 = new int[] { 2, 4, 6 };
 
             SelectionSort sort = new SelectionSort();
             int[] sorted = sort.SelSort(BS);
 
+            Divide_and_Conquer arr = new Divide_and_Conquer();
+
+            int sum = arr.Sum(sorted, sorted.Length);
+            Console.WriteLine(sum);
         }
     }
 }
